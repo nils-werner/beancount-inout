35f643eb2877d8a4dc48cdc9e382fdbeb4754a45
Nils Werner <nils@hey.com>
2022-01-10 21:04:22 +0100

    Fix output formatting

--- beancount_inout/__init__.py
+++ beancount_inout/__init__.py
@@ -3,24 +3,36 @@
 import sys
 from datetime import datetime, date
 from decimal import Decimal
+from contextlib import contextmanager
 
 import click
 from beancount.core import data, flags, amount
 from beancount.parser import printer
 
+
+# Do this because of
+# https://github.com/pallets/click/issues/2018
+@contextmanager
+def to_stderr():
+    sys.stdout = sys.__stderr__
+    yield
+    sys.stdout = sys.__stdout__
+
+
 @click.command()
-@click.option('-i', '--income', help="The income (debited) account", default="Income:Work")
+@click.option('-i', '--income', help="The income (debited) account", default="Income:Work", show_default=True)
 @click.option('-p', '--payee', help="Payee line")
 @click.option('-s', '--subject', help="Subject line")
-@click.option('-r', '--rate', help="Hourly rate in 'units currency'")
-@click.option('-u', '--unit', help="Work unit", default="WKHR")
+@click.option('-r', '--rate', help="Hourly rate in units and currency, e.g. '60 USD'")
+@click.option('-u', '--unit', help="Work unit", default="WKHR", show_default=True)
 @click.option('-h', '--hours', help="Hours spent working", type=float)
 @click.argument('asset')
 def inout(income, payee, subject, rate, unit, hours, asset):
     starttime = datetime.now()
 
     if subject is None:
-        subject = click.prompt('Subject', default="", err=True)
+        with to_stderr():
+            subject = click.prompt('Subject', default="", err=False)
 
     txn = data.Transaction(
         date=date.today(),
@@ -35,7 +47,8 @@ def inout(income, payee, subject, rate, unit, hours, asset):
 
     if hours is None:
         try:
-            click.confirm('Finish work', default=True, err=True)
+            with to_stderr():
+                click.confirm('Finish work', default=True, err=False)
         except KeyboardInterrupt:
             sys.exit(1)
 
@@ -66,4 +79,7 @@ def inout(income, payee, subject, rate, unit, hours, asset):
         )
     )
 
-    printer.print_entry(txn)
+    # Do this to print line before, not after
+    out = printer.format_entry(txn).strip()
+    print()
+    print(out)
